32e90a1fd4ea6730142305022cd6034daafe8b8b
Smska23 <134395872+Smska23@users.noreply.github.com>
2023-10-25 11:47:16 +0300

    second

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         out(true,false,true);
         out(true,true,false);
         out(true,true,true);
-        //developer ,l,l,l,l,l,l,l,l,ll,l,l,l,l
+        //developer ,l,l,l,l,l,l,l,l,ll,l,l,l,lkkkkk
     }
     public static void out(boolean x, boolean y, boolean z){
         System.out.println((x ? "1 " : "0 ") + (y ? "1 " : "0 ") + (z ? "1 " : "0 ") + ((x && y && z) ^ (x || !(y && z)) ^ (x && y || !z) ^ (x && !y && z) ? " 1" : " 0"));
